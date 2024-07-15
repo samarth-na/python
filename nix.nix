fa24640ea42a30cbc72aa359f7e428ce75034a0c
samarth-nagar <samarthxma2009@gmail.com>
2024-07-15 15:42:35 +0530

    backup

--- nix.nix
+++ nix.nix
@@ -4,5 +4,8 @@ let
     hello = "hello world";
 in
     print hello
+
+    for i in hello
+        print i     
 #this works by default because nixpkgs is installed
 # why does this exist what was i thinking????

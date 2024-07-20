ee1e8cc2c3c374c5736aab7f128743ab260c6898
samarth-nagar <samarthxma2009@gmail.com>
2024-07-20 22:05:34 +0530

    backup

---
+++ basics-py/oju.py
@@ -0,0 +1,58 @@
+# variables
+name = "oju"
+print(name)
+age = 16
+
+print(age)
+inschool = True
+print(inschool)
+
+print("")
+# opertor
+newage = age + 1
+print(newage)
+
+fullname = name + "nagar"
+print(fullname)
+
+print("")
+# confitional
+
+# if (true ){
+# do this
+# }
+# if
+if age > 18:
+    print("adult")
+
+if age < 18:
+    print("child")
+
+print("")
+
+# else
+if age > 18:
+    print("adult")
+else:
+    print("child")
+
+print("")
+
+# elif
+if age > 18:
+    print("adult")
+elif age < 18:
+    print("child")
+else:
+    print("of age 18")
+
+print("")
+# loop
+# for
+for i in range(10):
+    print(i, "j")
+
+for i in range(10, 2, -1):
+    print(i, "j")
+
+# function
